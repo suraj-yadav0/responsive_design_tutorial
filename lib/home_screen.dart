6028bbf8fdae07055d223d23726b442b234dda75
Suraj Yadav <surajyadav200701@gmail.com>
2024-03-19 19:33:12 +0530

    now learnt about a more sophesticated way

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_design_tutorial/responsive/desktop_body.dart';
+import 'package:responsive_design_tutorial/responsive/mobile_body.dart';
+import 'package:responsive_design_tutorial/responsive/responsive_design.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,11 +14,13 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final currentWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      backgroundColor: currentWidth > 700 ? Colors.deepPurple : Colors.deepOrange,
-      body: Center(
-        child: Text(currentWidth.toString()),
-      ),
+    return const Scaffold(
+      // backgroundColor: currentWidth > 700 ? Colors.deepPurple : Colors.deepOrange,
+      // body: Center(
+      //   child: Text(currentWidth.toString()),
+      // ),
+
+      body: ResponsiveLayout(mobileBody: MyMobileBody(), desktopBody: MyDesktopBody()),
     );
   }
 }
